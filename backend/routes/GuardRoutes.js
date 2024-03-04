407d8cdb32da89876d2e67fd3ec16d1fa4766768
Azmat Yusuf <azmatyusuf2022@gmail.com>
2024-03-04 19:48:32 +0530

    added guard routes POST, GET

--- backend/routes/GuardRoutes.js
+++ backend/routes/GuardRoutes.js
@@ -4,26 +4,40 @@ import { Guard } from '../models/GuardModel.js';
 const router = express.Router();
 
 //route to create a new account
-router.post('/', async(request, response) => {
-    try{
+router.post('/', async (request, response) => {
+    try {
+        if (
+            !request.body.name ||
+            !request.body.EmpId
+        ) {
+            return response.status(400).send({
+                message: 'send all required fields: name , Emp Id'
+            });
+        }
+        const newEmp = {
+            name: request.body.name,
+            EmpId: request.body.EmpId,
+        };
 
-    }catch(error){
+        const guard = await Guard.create(newEmp);
+        return response.status(201).send(guard);
+    } catch (error) {
         console.log(error.message);
-        return response.status(500).send({message:error.message});
+        return response.status(500).send({ message: error.message });
     }
 
 });
 
 //Route to get a specific Guard
 router.get("/:id", async (request, response) => {
-    try{
-        const {id} = request.params; //get id from the request parameter
+    try {
+        const { id } = request.params; //get id from the request parameter
         const guard = await Guard.findById(id);
         return response.status(200).json(guard);
 
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
-        response.status(500).send({message: error.message});
+        response.status(500).send({ message: error.message });
     }
 });
 
