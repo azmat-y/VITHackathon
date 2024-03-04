de6aca54d04e1fb71d3ec5ede773754306be8ca9
Azmat Yusuf <azmatyusuf2022@gmail.com>
2024-03-04 19:27:43 +0530

    fixed typos

--- backend/index.js
+++ backend/index.js
@@ -3,21 +3,23 @@ import { PORT, MongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import GuardRoutes from './routes/GuardRoutes.js';
+import StudentRoutes from './routes/StudentRoutes.js';
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
 app.use('/guard', GuardRoutes);
+app.use('/student', StudentRoutes);
 
 mongoose
     .connect(MongoDBURL)
-    .then(()=>{
+    .then(() => {
         console.log("App connected to MongoDB");
         app.listen(PORT, () => {
             console.log(`app is listening to port: ${PORT}`);
         });
     })
-    .catch((error)=>{
+    .catch((error) => {
         console.log(error);
     });
--- backend/routes/StudentRoutes.js
+++ backend/routes/StudentRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Student } from '../models/StudentModel';
+import { Student } from '../models/StudentModel.js';
 
 const router = express.Router();
 
